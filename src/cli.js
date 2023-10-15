381d8bf1605d1ba626e45220a20661e7e8796959
Anton Zakharov <hello@antonzakharov.ru>
2023-10-15 23:27:22 +0300

    Init eslint and fix the code

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const greeting = () => {
-    const name = readlineSync.question('May I have your name? ');
+  const name = readlineSync.question('May I have your name? ');
 
-    console.log(`Hello, ${name}!`);
-}
+  console.log(`Hello, ${name}!`);
+};
 
-export {greeting};
+export default { greeting };
