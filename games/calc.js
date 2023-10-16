50ab67b76655893fdc0220d1eae519c34a138dbc
Anton Zakharov <hello@antonzakharov.ru>
2023-10-16 21:04:43 +0300

    Add calc game

---
+++ games/calc.js
@@ -0,0 +1,26 @@
+import playGame from '../src/index.js';
+
+const maxValue = 99;
+const rules = 'What is the result of the expression?';
+const operations = ['+', '-', '*'];
+
+const getNumber = () => Math.floor(Math.random() * maxValue);
+const getOperation = () => operations[Math.floor(Math.random() * operations.length)];
+
+const getQuestion = () => `${getNumber()} ${getOperation()} ${getNumber()}`;
+const getSolution = (question) => {
+  const [a, operation, b] = question.split(' ');
+  switch (operation) {
+    case '+':
+      return `${parseInt(a, 10) + parseInt(b, 10)}`;
+    case '-':
+      return `${parseInt(a, 10) - parseInt(b, 10)}`;
+    case '*':
+      return `${parseInt(a, 10) * parseInt(b, 10)}`;
+    default:
+      console.log('unsupported operation');
+      return '';
+  }
+};
+
+export default () => playGame(rules, getQuestion, getSolution);
