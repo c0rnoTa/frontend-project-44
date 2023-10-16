a9c7983d2fe6cd01ffbcfb07d177b22072cac598
Anton Zakharov <hello@antonzakharov.ru>
2023-10-17 00:51:25 +0300

    Another export default

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -2,4 +2,6 @@
 
 import greeting from '../src/cli.js';
 
+console.log('Welcome to the Brain Games!');
+
 greeting();
--- src/cli.js
+++ src/cli.js
@@ -1,11 +1,7 @@
 import readlineSync from 'readline-sync';
 
-const greeting = () => {
-  console.log('Welcome to the Brain Games!');
-
+export default () => {
   const name = readlineSync.question('May I have your name? ');
 
   console.log(`Hello, ${name}!`);
 };
-
-export default { greeting };
