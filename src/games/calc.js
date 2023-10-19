765f6c90cca22c4e20c95f158359e72453406dc3
Anton Zakharov <hello@antonzakharov.ru>
2023-10-20 00:49:10 +0300

    Separate calc function

--- src/games/calc.js
+++ src/games/calc.js
@@ -5,22 +5,27 @@ const maxValue = 99;
 const rules = 'What is the result of the expression?';
 const operations = ['+', '-', '*'];
 
+const calc = (a, operation, b) => {
+  switch (operation) {
+    case '+':
+      return a + b;
+    case '-':
+      return a - b;
+    case '*':
+      return a * b;
+    default:
+      console.log('unsupported operation');
+      return null;
+  }
+};
+
 const getOperation = () => operations[getRandomNumber(operations.length)];
 
 const getQuestion = () => `${getRandomNumber(maxValue)} ${getOperation()} ${getRandomNumber(maxValue)}`;
 const getSolution = (question) => {
   const [a, operation, b] = question.split(' ');
-  switch (operation) {
-    case '+':
-      return `${parseInt(a, 10) + parseInt(b, 10)}`;
-    case '-':
-      return `${parseInt(a, 10) - parseInt(b, 10)}`;
-    case '*':
-      return `${parseInt(a, 10) * parseInt(b, 10)}`;
-    default:
-      console.log('unsupported operation');
-      return '';
-  }
+  const calcutaionResult = calc(parseInt(a, 10), operation, parseInt(b, 10));
+  return `${calcutaionResult}`;
 };
 
 export default () => playGame(rules, getQuestion, getSolution);
